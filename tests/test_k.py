13e075161d46fe161a321bb0b3a54931f1b5f0f0
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-01 01:39:30 +0100

    two more tests

--- tests/test_k.py
+++ tests/test_k.py
@@ -14,8 +14,20 @@ class TestK(unittest.TestCase):
     def test_type(self):
         x = k.make("x", "a")
         self.assertIsInstance(x.a, x)
-
+        self.assertIsInstance(x.a, int)
 
     def test_type_name(self):
         A = k.make("Foobar", "X")
         self.assertEquals(str(type(A.X)), "<class 'k.Foobar'>")
+
+    def test_values(self):
+        A = k.make("A", "xyz")
+        self.assertEquals(A.x, 0)
+        self.assertEquals(A.y, 1)
+        self.assertEquals(A.z, 2)
+
+    def test_values_with_start(self):
+        start = 42
+        A = k.make("A", "abcd", start=start)
+        self.assertEquals(A.a, start)
+        self.assertEquals(A.d, start+3)
